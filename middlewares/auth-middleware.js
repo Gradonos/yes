f4ee24e2cbde7c84de2adb6759220cf429b53a3f
Gradonos <96689370+Gradonos@users.noreply.github.com>
2023-01-17 12:28:56 +0300

    Update auth-middleware.js

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -5,22 +5,22 @@ module.exports = function (req, res, next) {
     try {
         const authorizationHeader = req.headers.authorization;
         if (!authorizationHeader) {
-            return next(ApiError.UnauthorizedError());
+            return next(ApiError.UnauthorizedError("header"));
         }
 
         const accessToken = authorizationHeader.split(' ')[1];
         if (!accessToken) {
-            return next(ApiError.UnauthorizedError());
+            return next(ApiError.UnauthorizedError("access"));
         }
 
         const userData = tokenService.validateAccessToken(accessToken);
         if (!userData) {
-            return next(ApiError.UnauthorizedError());
+            return next(ApiError.UnauthorizedError("userdata"));
         }
 
         req.user = userData;
         next();
     } catch (e) {
-        return next(ApiError.UnauthorizedError());
+        return next(ApiError.UnauthorizedError("hz"));
     }
 };
