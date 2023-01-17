9abbe411302b3b3bc3493b4b015f630db454637f
Gradonos <96689370+Gradonos@users.noreply.github.com>
2023-01-17 13:14:43 +0300

    Update auth-middleware.js

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -3,21 +3,6 @@ const tokenService = require('../service/token-service');
 
 module.exports = function (req, res, next) {
     try {
-        const authorizationHeader = req.headers.authorization;
-        if (!authorizationHeader) {
-            return next(ApiError.UnauthorizedError("header"));
-        }
-
-       const token = req.headers["authorization"];
-        if (!accessToken) {
-            return next(ApiError.UnauthorizedError("access"));
-        }
-
-        const userData = tokenService.validateAccessToken(accessToken);
-        if (!userData) {
-            return next(ApiError.UnauthorizedError("userdata"));
-        }
-
         req.user = userData;
         next();
     } catch (e) {
